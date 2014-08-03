7831d1d5634db1f9ed7a10189452f15c120b5517
Mike Boers <github@mikeboers.com>
2014-08-03 11:10:19 -0700

    Convert Flask-ACL from subtree to submodule

--- lib/Flask-ACL/flask_acl/__init__.py
+++
@@ -1,2 +0,0 @@
-from .core import AuthManager, check
-from .globals import current_auth
--- lib/Flask-ACL/flask_acl/globals.py
+++
@@ -1,7 +0,0 @@
-import functools
-
-import werkzeug as wz
-from flask import current_app
-
-# Proxy to the current app's AuthManager
-current_auth = wz.local.LocalProxy(lambda: current_app.auth_manager)
