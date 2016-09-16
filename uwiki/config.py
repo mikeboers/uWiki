eacf473aec198c7a4670113878da03a0a15c5c1a
Mike Boers <github@mikeboers.com>
2016-09-16 07:02:51 -0400

    Support an instance config at var/config.py

--- uwiki/config.py
+++ uwiki/config.py
@@ -1,21 +1,22 @@
 import os
 
-# This is the static config.
 
 ROOT_PATH = os.path.abspath(os.path.join(__file__, '..', '..'))
 INSTANCE_PATH = os.path.join(ROOT_PATH, 'var')
 
-
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(INSTANCE_PATH, 'db.sqlite')
 SQLALCHEMY_TRACK_MODIFICATIONS = False # Soon to be default behaviour.
 
-
-# TODO: Set this better.
 SECRET_KEY = 'monkey'
-DEBUG = True
-
 
 LDAP_URL = None
-LDAP_URL = 'ldap://ldap.mm'
 LDAP_USER_DN = 'uid=%s,ou=people,dc=mm'
 LDAP_GROUP_ROOT = 'ou=group,dc=mm'
+
+
+
+
+# If there is an instance static config, use it too.
+instance_config = os.path.join(INSTANCE_PATH, 'config.py')
+if os.path.exists(instance_config):
+    execfile(instance_config)
