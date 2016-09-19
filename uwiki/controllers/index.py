87366c35af269f91df7ac87d0379e45a3e564af3
Mike Boers <mikeb@markmedia.co>
2016-09-19 16:23:46 -0400

    Expose a page index

--- uwiki/controllers/index.py
+++ uwiki/controllers/index.py
@@ -1,5 +1,16 @@
 from . import *
 
 @app.route('/')
-def index():
+def redirect_to_index():
     return redirect(url_for('page', name='Index'))
+
+
+@app.route('/index')
+def index():
+
+    pages = Page.query.all()
+    pages.sort(key=lambda page: page.title)
+    pages = [p for p in pages if authz.can('page.list', p)]
+
+    return render_template('/page/index.haml', pages=pages)
+    
